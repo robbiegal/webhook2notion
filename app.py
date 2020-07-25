a758fcc585b5eb6435499424f4a3c14b7836741d
robbie.galfrin <robbie.galfrin@argus-sec.com>
2020-07-25 22:39:51 +0300

    time calculation bug fix

--- app.py
+++ app.py
@@ -18,9 +18,9 @@ def createCalendarEvent(TOKEN, collectionURL, description, event_begins, event_e
     row.Action_Item = summary
     row.Status = "Active"
     row.Priority = "Scheduled"
-    row.Note =  ((("Location: "+location) if (location!=None) else "" ) + "Description: "+str(description)
+    row.Note =  (("Location: "+location) if (location!=None) else "" ) + "Description: "+str(description)
     #handle start time:
-    #print("Received event with timestamp of " +str(event_begins)+'\n')
+    print("Received event with timestamp of " +str(event_begins)+'\n')
     event_start_params = re.split('[-T:+]',event_begins)
     event_start_params=[int(i) for i in event_start_params]
     esp = event_start_params
